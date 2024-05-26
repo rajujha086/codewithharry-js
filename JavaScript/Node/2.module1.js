911a17069920df8cedef7ec30e3492788cadd0af
raju-jha <rajivjha.admec@gmail.com>
2024-05-26 17:35:34 +0530

    javaScript code

---
+++ JavaScript/Node/2.module1.js
@@ -0,0 +1,14 @@
+const hello = () => {
+  console.log("Hello Raju")
+}
+
+const ahello = (name) => {
+  console.log("Hello " + name)
+}
+
+// module.exports = { hello, ahello }; // same as below line
+// module.exports = { hello: hello, ahello: ahello }; 
+
+export default harry = () => {
+  console.log("Hello " + "Harry")
+}
---
+++ JavaScript/Node/97_modules.js
@@ -0,0 +1,15 @@
+// const hello = require("./2.module1")
+// hello.hello()
+// hello.ahello("Jha")
+// hello.ahello("Shivani")
+// hello.ahello("Nitika")
+
+
+
+// const { hello, ahello } = require("./2.module1")
+import harry, { hello, ahello } from "./2.module1.js"
+hello()
+ahello("Jha")
+ahello("Shivani")
+ahello("Nitika")
+harry()
